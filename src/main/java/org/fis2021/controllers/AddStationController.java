972350ded9b38f0d4b53aa0678a70d2d76e14d8e
PaulM <paulmicluta@gmail.com>
2021-05-08 15:27:51 +0300

    ECSA-26 - Pop up window for adding stations created

---
+++ src/main/java/org/fis2021/controllers/AddStationController.java
@@ -0,0 +1,27 @@
+package org.fis2021;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AddStationController {
+
+    @FXML
+    private TextField companyNameTextField;
+
+    @FXML
+    private TextField stationNameTextField;
+
+    @FXML
+    private TextField cityTextField;
+
+    @FXML
+    private TextField addressTextField;
+
+    @FXML
+    private Button addStationButton;
+
+    @FXML
+    private Button goBackToHomePageButton;
+
+}
