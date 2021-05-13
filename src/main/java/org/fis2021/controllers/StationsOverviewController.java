904460db7d745fdd4cc5792241fe913b004660ca
PaulM <paulmicluta@gmail.com>
2021-05-13 18:53:11 +0300

    ECSA-37 - Overview GUI created

---
+++ src/main/java/org/fis2021/controllers/StationsOverviewController.java
@@ -0,0 +1,21 @@
+package org.fis2021.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+public class StationsOverviewController {
+
+    @FXML
+    private Label companyName;
+
+    @FXML
+    private Label totalStations;
+
+    @FXML
+    private ListView<?> listView;
+
+
+
+}
+
