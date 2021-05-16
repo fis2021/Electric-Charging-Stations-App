38aefee565723a3a6e633c56193ca50a7590c702
Darius Firan <batmanslappsbutter.17@gmail.com>
2021-05-16 17:26:29 +0300

    ECSA-29 Created button for report and also added the stations adress in station scene

--- src/main/java/org/fis2021/controllers/StationController.java
+++ src/main/java/org/fis2021/controllers/StationController.java
@@ -1,8 +1,16 @@
 package org.fis2021.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import org.dizitart.no2.objects.ObjectRepository;
 import org.fis2021.ApplicationHelper;
@@ -10,11 +18,16 @@ import org.fis2021.model.Stations;
 import org.fis2021.services.StationsService;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Optional;
 
 import static org.fis2021.App.loadFXML;
 
 
 public class StationController {
+
+    @FXML
+    private AnchorPane stationAnchorPane;
 
     @FXML
     private ComboBox<Integer> hourBox;
@@ -37,6 +50,9 @@ public class StationController {
     @FXML
     private Label stationNameLabel;
 
+    @FXML
+    private Label stationStreetLabel;
+
 
     private final Integer[] hour = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     private final Integer[] min = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
@@ -50,6 +66,32 @@ public class StationController {
         minuteBox.getItems().addAll(min);
         secondBox.getItems().addAll(sec);
         stationNameLabel.setText(ApplicationHelper.stationName);
+        Stations station = StationsService.getExactStationFromCity(ApplicationHelper.stationName, ApplicationHelper.stationCity);
+        if (station != null) {
+            stationStreetLabel.setText(station.getAddress());
+        }
+
+        AnchorPane root = stationAnchorPane;
+
+        Button reportButton = new Button();
+
+        reportButton.setText("Report malfunction");
+        reportButton.setStyle("-fx-background-color: #ff0000; -fx-border-color: #990000; -fx-text-fill: WHITE");
+        reportButton.setAlignment(Pos.CENTER);
+        reportButton.wrapTextProperty().setValue(true);
+
+        reportButton.setPrefHeight(45.0);
+        reportButton.setPrefWidth(135.0);
+        reportButton.setLayoutX(250.0);
+        reportButton.setLayoutY(280.0);
+
+        Image image = new Image("pictures/malfunction.png");
+        ImageView iview = new ImageView(image);
+        iview.setFitHeight(40);
+        iview.setPreserveRatio(true);
+        reportButton.setGraphic(iview);
+
+        root.getChildren().add(reportButton);
     }
 
     public void okButtonOnAction() throws IOException {
